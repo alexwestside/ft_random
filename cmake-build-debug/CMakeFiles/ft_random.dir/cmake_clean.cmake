a4a7a21f8e923fdea411addfb26d4d69ee5e8234
orizhiy <sklif.85@gmail.com>
2017-03-01 12:58:14 +0200

    rand_v.0

---
+++ cmake-build-debug/CMakeFiles/ft_random.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ft_random.dir/main.cpp.o"
+  "CMakeFiles/ft_random.dir/main.c.o"
+  "ft_random.pdb"
+  "ft_random"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/ft_random.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
